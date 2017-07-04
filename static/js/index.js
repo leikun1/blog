ecd00073b32bdad131cc69a0234ccc75159dce6c
Grace.Keven <415397381@qq.com>
2017-07-05 00:43:16 +0800

    queryData
    queryData

--- static/js/index.js
+++ static/js/index.js
@@ -5,12 +5,14 @@
     })
 
     var index = function(){
+       this.queryArticleTypeUrl = "http://www.leikun01.com/connection/mysql.php";
        this.bg_music = $("#bg_music")[0];  //背景音乐
        this.bg_music_flag = false;
     }
 
     index.prototype.init = function(){
        if(this.bg_music_flag)this.utils().playmusic();
+       this.utils().queryArticleType();
        this.bindEvents();
     }
     //事件绑定
@@ -25,6 +27,11 @@
           window.setTimeout(function(){
             _this.bg_music.play();
           },1000);
+        }
+        utils.prototype.queryArticleType = function(){
+          $.post(_this.queryArticleTypeUrl,{},function(data){
+              console.log(JSON.stringify(data));
+          });
         }
         return new utils();
     }
--- static/js/silder.js
+++ static/js/silder.js
@@ -1,10 +1,7 @@
 (function($){
   //根据当前路径加载active样式
   var obj = $("#topnav a")[0];
-  console.log(obj);
   $("#topnav a").each(function(index){
-      console.log(this.href);
-        console.log(window.location.href.indexOf(this.href));
        if(window.location.href.indexOf(this.href) >= 0){
          obj = this;
          return false;
