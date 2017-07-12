1205a58d82fad84ff1354e944e8b51747c1cab71
Grace.Keven <415397381@qq.com>
2017-07-12 16:45:41 +0800

    集成服务器
    集成服务器

--- server/articletype.php
+++ server/articletype.php
@@ -1,6 +1,4 @@
 <?php
-
-define(DB_TABLENAME, 'articletype');
 class ArticleType{
   public $articleTypePK;
   public $articleTypeName;
@@ -10,39 +8,58 @@ class ArticleType{
   public $articleTypePPK;
   public $articleTypeSatus;
   public $articleTypeRemarks;
-}
 
-//数据库表的列名
-$dbcolarray = array('articleTypePK','articleTypeName', 'articleTypeValue'
-,'articleTypeBrief','articleTypeLogo','articleTypePPK','articleTypeSatus','articleTypeRemarks');
-//$pagearray = array('currentPage','queryNum');
-
-$conditions = "";
-
-$params = array();
-//项目条目
-foreach ($dbcolarray as $col){
-  Array_push($params, $col);
-}
-
-//$params = $_POST["params"];
-foreach ($params as $p){
-    if(!empty($_POST[$p])){
-       if(empty($conditions)){
-         $conditions ="where ".$p." = '".$_POST[$p]."'";
-       }else{
-         $conditions .=" and ".$p." = '".$_POST[$p]."'";
-       }
+  //拼接sql
+  function getsql(){
+    define(DB_TABLENAME, 'articletype');
+    //数据库表的列名
+    $dbcolarray = array('articleTypePK','articleTypeName', 'articleTypeValue'
+    ,'articleTypeBrief','articleTypeLogo','articleTypePPK','articleTypeSatus','articleTypeRemarks');
+    //查询条件
+    $conditions = "";
+    $params = array();
+    //项目条目
+    foreach ($dbcolarray as $col){
+      Array_push($params, $col);
     }
+    //查询条目
+    foreach ($params as $p){
+        if(!empty($_POST[$p])){
+           if(empty($conditions)){
+             $conditions ="where ".$p." = '".$_POST[$p]."'";
+           }else{
+             $conditions .=" and ".$p." = '".$_POST[$p]."'";
+           }
+        }
+    }
+    //order by
+    $order = $_POST["order"];
+    if(!empty($order))$conditions.="order by".$order;
+    //分页
+    if(!empty($limitStr))$conditions.=$limitStr;
+    //添加引号
+    if(!(strpos($conditions,";") !== false)){
+      $conditions.=";";
+    }
+    $sql = sprintf("select %s from %s %s", implode(",",$dbcolarray), DB_TABLENAME,$conditions);
+
+    echo $sql;
+    return $sql;
+  }
+
+  //设置结果集
+  function getvalue($row){
+    $t=new ArticleType();
+    $t->articleTypePK=$row->articleTypePK;
+    $t->articleTypeName=$row->articleTypeName;
+    $t->articleTypeValue=$row->articleTypeValue;
+    $t->articleTypeBrief=$row->articleTypeBrief;
+    $t->articleTypeLogo=$row->articleTypeLogo;
+    $t->articleTypePPK = $row->articleTypePPK;
+    $t->articleTypeSatus = $row ->articleTypeSatus;
+    $t->articleTypeRemarks=$row->articleTypeRemarks;
+    return $t;
+  }
 }
-
-if(!empty($limitStr))$conditions.=$limitStr;
-
-if(!(strpos($conditions,";") !== false)){
-  $conditions.=";";
-}
-
-$sql = sprintf("select %s from %s %s", implode(",",$dbcolarray), DB_TABLENAME,$conditions);
-//echo $sql;
 
 ?>
---
+++ server/server.php
@@ -0,0 +1,24 @@
+<?php
+include 'const.php';  //引入常量代码
+include 'articletype.php';  //引入常量代码
+
+//mysql_connect
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS,DB_DATABASENAME) or die("connect failed" . mysql_error());
+//读取表中纪录条数
+mysqli_query($conn,"set names 'utf8'"); //设置编码
+$action = $_POST["action"];
+//获取sql
+$data = new $action();
+$result = mysqli_query($conn,$data->getsql()); //sql
+$r = new Result(); //结果集
+$valueArr=array();
+while($row=mysqli_fetch_object($result)){
+    if(!empty($data))$valueArr[]=$data->getvalue($row);
+}
+$r->value = $valueArr;
+//$valueArr;
+
+echo json_encode($r,JSON_UNESCAPED_UNICODE);
+mysqli_free_result($result);
+mysqli_close($conn);
+?>
