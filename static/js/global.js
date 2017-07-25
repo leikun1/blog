b9f81733d96425245a4789c27cc10a582988eff0
Grace.Keven <415397381@qq.com>
2017-07-25 16:09:21 +0800

    ll
    ll

--- static/js/global.js
+++ static/js/global.js
@@ -9,7 +9,7 @@
       //"http://www.leikun01.com/server/server.php",
       "http://localhost/blogH5/server/server.php",
     }),
-    GetQueryString : function(name){
+    getQueryString : function(name){
       var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)","i");
     	var r = window.location.search.substr(1).match(reg);
     	if (r!=null) return decodeURI((r[2])); return null;
--- static/js/learn.js
+++ static/js/learn.js
@@ -7,14 +7,15 @@
     var learn = function(){
        this.learn_article_typelist = $(".learn_article_typelist");
        this.article_detail_url = "../page/new.html";
-       this.index_article_recommend = $(".index_article_recommend");
-       this.index_article_newest = $(".index_article_newest");
-       this.index_article_view = $(".index_article_view");
-       this.article_detail_btnStr = ".article_detail_btn";       
+       this.learn_article_recommend = $(".learn_article_recommend");
+       this.learn_article_newest = $(".learn_article_newest");
+       this.learn_article_view = $(".learn_article_view");
+       this.article_detail_btnStr = ".article_detail_btn";
     }
 
     learn.prototype.init = function(){
        this.utils().queryArticleType();
+       this.utils().article();
        this.bindEvents();
     }
 
@@ -41,6 +42,72 @@
                 }
               }
               _this.learn_article_typelist.html(html);
+          });
+        }
+        utils.prototype.article = function(){
+            this.articleRecommend();
+            this.articleNewest();
+            this.articleView();
+        }
+        utils.prototype.articleRecommend = function(){
+          $.post({
+            action:"article",keyword:"getSimpleArr",articleRecommend:"1"
+         },function(data){
+             var article_recommend_html = "";
+             if(data.value && data.value.length > 0){
+               for(var i = 0; i < data.value.length; i++ ){
+                  article_recommend_html+=
+                  "<h2>"+(data.value[i].articleTitle ? data.value[i].articleTitle : "")+"</h2>"+
+                  "<p class=\"dateview\"><span>发布时间："+
+                  (data.value[i].articleAddTime?data.value[i].articleAddTime:"--")+
+                  "</span><span>作者："+(data.value[i].articleAuthor?data.value[i].articleAuthor:"--")+
+                  "</span><span>分类：[<a href='javascript:;'>"+(data.value[i].articleProperty == '1'
+                  ? "原创" : data.value[i].articleProperty == '2' ? "转载" : "--")+"</a>]</span></p>"+
+                  "<figure><img src=\"../static/images/01.jpg\"></figure>"+
+                  "<ul class=\"nlist\">"+
+                  "<p>"+(data.value[i].articleRemarks?data.value[i].articleRemarks:"")+"</p>"+
+                  "<a title=\"阅读全文\" href='javascript:;' data-pk='"+data.value[i].articlePK+
+                  "' class=\"readmore article_detail_btn\">详细信息>></a>"+
+                  "</ul>"+
+                  "<div class=\"line\"></div>";
+               }
+             }
+             article_recommend_html += "<div class=\"blank\"></div>"+
+             "<div class=\"page\">"+
+             "<b>1</b>"+
+             "<a href='javascript:;' >2</a>"+
+             "<a href='javascript:;' >&gt;</a>"+
+             "<a title=\"Total record\">"+
+             "<b>41</b>"+
+             "</a>"+
+             "<a href='javascript:;' >&gt;&gt;</a></div>";
+             _this.learn_article_recommend.html(article_recommend_html);
+          });
+        }
+        utils.prototype.articleNewest = function(){
+          $.post({action:"article",keyword:"getSimpleArr",order:"articleViewTimes desc"},function(data){
+            var article_newest_html = "";
+            if(data.value && data.value.length > 0){
+              for(var i = 0; i < data.value.length; i++ ){
+                var articleTitle = (data.value[i].articleTitle ? data.value[i].articleTitle : "**");
+                 article_newest_html+="<li><a href='javascript:;' title='"+articleTitle+
+                 "' data-pk='"+data.value[i].articlePK+"' class=\"article_detail_btn\" >"+articleTitle+"</a></li>";;
+              }
+            }
+            _this.learn_article_newest.html(article_newest_html);
+          });
+        }
+        utils.prototype.articleView = function(){
+          $.post({action:"article",keyword:"getSimpleArr",order:"articleAddTime desc"},function(data){
+            var article_view_html = "";
+            if(data.value && data.value.length > 0){
+              for(var i = 0; i < data.value.length; i++ ){
+                var articleTitle = (data.value[i].articleTitle ? data.value[i].articleTitle : "**");
+                 article_view_html+="<li><a href='javascript:;' title='"+articleTitle+
+                 "' data-pk='"+data.value[i].articlePK+"' class=\"article_detail_btn\" >"+articleTitle+"</a></li>";;
+              }
+            }
+            _this.learn_article_view.html(article_view_html);
           });
         }
         return new utils();
