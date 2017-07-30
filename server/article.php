8d2cddf6eb4331dad531f654c0d72b1b715417fd
Grace.Keven <415397381@qq.com>
2017-07-31 00:18:02 +0800

    a
    a

--- server/article.php
+++ server/article.php
@@ -17,7 +17,7 @@ class article{
   //获取简单字符串
   function getSimpleArr(){
     $dbcolarray = array('articlePK','articleTitle', 'articleLittleTitle'
-    ,'articleAuthor','articleAddTime','articleLabel','articleTypePK','articleRemarks'
+    ,'articleAuthor','articleAddTime','articleLabel','ac.`articleTypePK`','articleRemarks'
     ,'articleProperty','articleStatus','articleRecommend','articleViewTimes');
     return $dbcolarray;
   }
@@ -35,12 +35,21 @@ class article{
   //拼接sql
   function getsql($keyword){
     define(DB_TABLENAME, 'article');
+    define(JOIN_TABLENAME, 'articletype');
     //查询条件
     $conditions = "";
     if(empty($keyword))$keyword = 'getAllArr';
     $dbcolarray = $this->$keyword();
+    if(!empty($_POST["articleTypePK"]) && !empty($_POST["articleTypePPK"])){
+       $conditions = "where (ac.`articleTypePK` ='".$_POST["articleTypePK"]."' or  act.`articleTypePPK` = '".$_POST["articleTypePPK"]."')";
+    }else if(!empty($_POST["articleTypePK"])){
+       $conditions = "where ac.`articleTypePK` ='".$_POST["articleTypePK"]."'";
+    }else if(!empty($_POST["articleTypePPK"])){
+       $conditions = "where act.`articleTypePPK` ='".$_POST["articleTypePPK"]."'";
+    }
     foreach ($dbcolarray as $p){
         if(!empty($_POST[$p])){
+           if($p == "articleTypePK" || $p == "articleTypePPK")continue;
            if(empty($conditions)){
              $conditions ="where ".$p." = '".$_POST[$p]."'";
            }else{
@@ -48,6 +57,7 @@ class article{
            }
         }
     }
+
     //order by
     $order = $_POST["order"];
     if(!empty($order))$conditions.="order by ".$order;
@@ -57,7 +67,8 @@ class article{
     if(!(strpos($conditions,";") !== false)){
       $conditions.=";";
     }
-    $sql = sprintf("select %s from %s %s", implode(",",$dbcolarray), DB_TABLENAME,$conditions);
+    $sql = sprintf("select %s from %s as ac left join %s as act ON ac.`articleTypePK`  = act.`articleTypePK` %s"
+    , implode(",",$dbcolarray), DB_TABLENAME,JOIN_TABLENAME,$conditions);
     //echo $sql;
     return $sql;
   }
